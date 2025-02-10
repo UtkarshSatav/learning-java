be857d67c28e916ec8f87f558bd5dade8d827f29
UtkarshSatav <ianutakrsh@gmail.com>
2025-02-10 14:53:09 +0530

    Changes made in demo2 file

--- Workshop/Demo2.java
+++ Workshop/Demo2.java
@@ -1,18 +1,41 @@
 import java.util.Scanner;
-public class Demo2 {
-    public static void main(String[] args) {
-        
+public class Demo2
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
         int num;
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        num = sc.nextInt();
-
+        System.out.println("Enter a number: ");
+        num = input.nextInt();
         if(num % 2 == 0)
-            System.out.println("Even number");
+        {
+            System.out.println("The number is even");
+        }
         else
-            System.out.println("Odd number");
+        {
+            System.out.println("The number is odd");
+        }
+        System.out.println("Enter a character: ");
+        char ch = input.next().charAt(0);
+        switch (ch) {
+            case 'a':
+                System.err.println("Vowel a");
+                break;
+            case 'e':
+                System.err.println("Vowel e");
+                break;
+            case 'i':
+                System.err.println("Vowel i");
+                break;
+            case 'o':
+                System.err.println("Vowel o");
+                break;
+            case 'u':
+                System.err.println("Vowel u");
+                break;
+            default:
+                System.err.println("Consonant");
+                break;
+        }
     }
-
 }
