6d8b12fd94186839d7c04310c1ec53b860901d0b
UtkarshSatav <ianutakrsh@gmail.com>
2025-02-14 10:45:27 +0530

    14-feb-2025

--- Lecture (Ritu Ma'am)/AbstractDemo.java
+++ Lecture (Ritu Ma'am)/AbstractDemo.java
@@ -10,7 +10,7 @@ class B extends A{
     void callme(){
         System.out.println("B's implementation of callme.");
     }
-}   
+}
 
 public class AbstractDemo {
     public static void main(String[] args) {
